9806d6ffebc1c3ee5ee4354015451d0ded77cf83
Master-ly <timroyal1999@gmail.com>
2019-09-25 18:18:02 +0800

    Update buyer.sol

--- contracts/buyer.sol
+++ contracts/buyer.sol
@@ -11,3 +11,6 @@ contract buyer {
 
     struct auctionstate {
         address cor;  // 拍卖品管理器, 负责拍卖品记录转移.
+        address dor;  // 资产管理器, 负责出价人的资产记录转移.
+        address per;  // 付款人 payer
+        uint256 amt;  // 卖方出价(FOR)
