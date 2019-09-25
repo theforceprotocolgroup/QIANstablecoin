7175fd3de7f6f86579a861cdb6913717cf3c10ae
Master-ly <timroyal1999@gmail.com>
2019-09-25 18:04:55 +0800

    Create and update buyer.sol

---
+++ contracts/buyer.sol
@@ -0,0 +1,13 @@
+//COPYRIGHT © THE FORCE PROTOCOL FOUNDATION LTD.
+//原力协议稳定币系统 - 稳定币购买
+
+pragma solidity >= 0.5.0;
+
+contract moveable {
+    function move(address from, address to, uint256 amt) public;
+}
+
+contract buyer {
+
+    struct auctionstate {
+        address cor;  // 拍卖品管理器, 负责拍卖品记录转移.
