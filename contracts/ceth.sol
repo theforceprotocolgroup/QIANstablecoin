a62efe6e80213994a5832a1dc623486e7f900e3f
Master-ly <timroyal1999@gmail.com>
2019-10-22 11:41:12 +0800

    Update ceth.sol

--- contracts/ceth.sol
+++ contracts/ceth.sol
@@ -8,3 +8,14 @@ import "./collateral.sol";
 
 contract ceth is collateral {
 
+    //@d dor
+    constructor(address d) collateral(d) public {
+
+    }
+
+    //存入 @msg.value 数量的抵押物
+    function deposit() public payable {
+        hol[msg.sender].c = uadd(hol[msg.sender].c, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
+
