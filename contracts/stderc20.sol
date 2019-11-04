cf88e8b2701232b4af24b0a66fc72a6d3a21f49f
Master-ly <timroyal1999@gmail.com>
2019-11-04 14:38:41 +0800

    Create stderc20.sol

---
+++ contracts/stderc20.sol
@@ -0,0 +1,54 @@
+//COPYRIGHT © THE FORCE PROTOCOL FOUNDATION LTD.
+//原力协议稳定币系统 - 标准ERC20实现
+
+/**  
+    Standard ERC20
+    https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+ */
+
+pragma solidity >= 0.5.0;
+
+import "./arith.sol";
+
+contract stderc20 is arith {
+    string  public name;
+    string  public symbol;
+    uint8   public decimals;
+    uint256 public totalSupply;                                         
+    mapping (address => uint)                      public balanceOf;    
+    mapping (address => mapping (address => uint)) public allowance;
+    
+    event Approval(address indexed owner, address indexed spender, uint amount);
+    event Transfer(address indexed from, address indexed to, uint amount);
+
+    constructor(string memory n, string memory s, uint8 d, uint256 t) public {
+        name = n;
+        symbol = s;
+        decimals = d;
+        totalSupply = t;
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address who, uint amount) public returns (bool) {
+        return transferFrom(msg.sender, who, amount);
+    }
+
+    function transferFrom(address from, address to, uint amount) public returns (bool)
+    {
+        require(balanceOf[from] >= amount);
+        if (from != msg.sender) {
+            require(allowance[from][msg.sender] >= amount);
+            allowance[from][msg.sender] = usub(allowance[from][msg.sender], amount);
+        }
+        balanceOf[from] = usub(balanceOf[from], amount);
+        balanceOf[to] = uadd(balanceOf[to], amount);
+        emit Transfer(from, to, amount);
+        return true;
+    }
+
+    function approve(address who, uint amount) public returns (bool) {
+        allowance[msg.sender][who] = amount;
+        emit Approval(msg.sender, who, amount);
+        return true;
+    }
+}
